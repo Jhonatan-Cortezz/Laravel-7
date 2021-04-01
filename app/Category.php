d6e6e9708c0acd0a3406ba7d80e820f99c1a08ce
Jhonatan Cortez <jhonatancortez708@gmail.com>
2021-04-01 13:36:24 -0600

    Funcionanmiento del softDeleted

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'name', 'module', 'slug',
     ];
+    protected $dates = ['deleted_at'];
+    protected $hidden= ['created_at', 'updated_at'];
 }
