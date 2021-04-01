8e54772c4b809de423a2452e8f8a42b975494101
Jhonatan Cortez <jhonatancortez708@gmail.com>
2021-04-01 17:11:45 -0600

    Configuracion de racion de Tag y product

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,82 +4,73 @@ namespace App\Http\Controllers;
 
 use App\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class TagController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
+        $tags = Tag::orderBy('id', 'desc')->paginate(15);
+        return view('admin.tags.index', compact('tags'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
+        return view('admin.tags.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'=>'required|unique:tags|max:20',
+            'module'=>'required|max:20'
+        ]);
+
+        $tag = new Tag;
+
+        // generate slug
+        $tag->name = e($request->name);
+        $tag->module = e($request->module);
+        $tag->slug = Str::slug($request->name);
+
+        $tag->save();
+
+        return redirect()->route('tags.index')->with('info', 'Agregado correctamente');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
     public function show(Tag $tag)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Tag $tag)
+    public function edit($id)
     {
-        //
+        $tag = Tag::where('id', $id)->firstOrFail();
+        return view('admin.tags.edit', compact('tag'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Tag $tag)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name'=>'required|max:20',
+            'module'=>'required|max:20'
+        ]);
+
+        $tag = Tag::findOrFail($id);
+        $tag->name = e($request->name);
+        $tag->module = e($request->module);
+        $tag->slug = Str::slug($request->name);
+
+        $tag->save();
+
+        return redirect()->route('tags.index')->with('info', 'Actualizado correctamente');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Tag $tag)
+    public function destroy($id)
     {
-        //
+        $tag = Tag::findOrFail($id)->delete();
+        return redirect()->route('tags.index')->with('info', 'Eliminado correctamente');
     }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'name', 'module', 'slug',
+    ];
+    protected $dates = ['deleted_at'];
+    protected $hidden= ['created_at', 'updated_at'];
+
+    // establecer la relacion a la tabla subcategoria
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
