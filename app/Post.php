2ef972bddca0d04170e95bf384e3ea34b4afbb6e
Jhonatan Cortez <jhonatancortez708@gmail.com>
2021-04-02 10:45:12 -0600

    Relacion polimorfica entre Image y Post

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'user_id', 'category_id', 'name','slug', 'abstract', 'body', 'status',
+    ];
+    protected $dates = ['deleted_at'];
+    protected $hidden= ['created_at', 'updated_at'];
+    //establece la relacion a la tabla Categoria
+    public function image(){
+        // belognsTo es la relacion inversa hacia el modelo categoria
+        return $this->morphOne('App\Image', 'imageable');
+    }
 }
